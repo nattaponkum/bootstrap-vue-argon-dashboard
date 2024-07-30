fc685e0ac67d40c0d24d9b85c558f2ceb71e757b
Nattapon Kumyaito <nattaponk@gmail.com>
2024-07-30 15:47:34 +0700

    get today data from egat-backend-01

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,8 +4,8 @@ import store from '@/store'
 export default () => {
     console.log('store.state.token', store.state.token)
     return axios.create({
-        baseURL: 'http://localhost:8081/',
-        // baseURL: 'https://egat-backend-01.azurewebsites.net/',
+        // baseURL: 'http://localhost:8081/',
+        baseURL: 'https://egat-backend-01.azurewebsites.net/',
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }

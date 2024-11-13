628612f26a422dfaf4393206ab77afebb0f0add7
Nattapon Kumyaito <nattaponk@gmail.com>
2024-11-13 11:01:54 +0700

    update Dashboard.vue

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,8 +4,8 @@ import store from '@/store'
 export default () => {
     console.log('store.state.token', store.state.token)
     return axios.create({
-        baseURL: 'http://localhost:8081/',
-        // baseURL: 'https://egat-pv-backend.azurewebsites.net/',
+        // baseURL: 'http://localhost:8081/',
+        baseURL: 'https://egat-pv-backend.azurewebsites.net/',
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
